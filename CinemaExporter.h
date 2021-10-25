5d07e5437fc808d43b99c56b8745214a81479b0e
Jonas Lukasczyk <jl@jluk.de>
2021-10-25 18:41:43 +0200

    CinemaExporter namespace

---
+++ CinemaExporter.h
@@ -0,0 +1,39 @@
+
+#include <iostream>
+
+#include <vtkm/cont/DataSet.h>
+
+namespace CinemaExporter {
+
+  // This method adds a global field to the dataset with a given underlying data type, name, and data
+  template<typename T>
+  void AddGlobalField(
+    vtkm::cont::DataSet& dataSet,
+    std::string name,
+    std::vector<T> vector
+  ){
+    dataSet.AddField(
+      vtkm::cont::Field(
+        name,
+        vtkm::cont::Field::Association::WHOLE_MESH,
+        vtkm::cont::make_ArrayHandle<T>(vector,vtkm::CopyFlag::On)
+      )
+    );
+  }
+
+  // This method returns a filename for a dataset by concatenating all 1-component field data values
+  std::string GetFileName(
+    vtkm::cont::DataSet& dataSet
+  );
+
+  // renders and writes images of the dataset
+  void RenderAndWrite(
+    int contourId,
+    vtkm::cont::DataSet& dataSet,
+    vtkm::Bounds& bounds,
+    int resX,
+    int resY,
+    int elevation0, int elevation1, int elevationStep,
+    int azimuth0, int azimuth1, int azimuthStep
+  );
+}
